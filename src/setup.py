c9e45f2d4297739d716f3faf376c6951d1e5850f
Gabriel Pehls <gabrielpehls@hotmail.com>
2023-05-28 00:49:39 -0300

    adjust setup dotpy

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Development of the phase 1 Tech Challenge @ Fiap',
+    author='Gabriel Pehls',
+    license='MIT',
+)
