49e3769c01a20232dbaf4334d323996ab74edadf
Gabriel Pehls <gabrielpehls@hotmail.com>
2023-06-21 10:43:45 -0300

    test

--- reports/src/get_data.py
+++ reports/src/get_data.py
@@ -40,7 +40,7 @@ def DF_NOAA_GLOBAL(years=15):
     return df
 
 import os
-raise Exception(os.listdir())
+raise Exception(os.listdir(rf'{base_path}\processed'))
 
 DF_VINHOS = pd.read_csv(rf'{base_path}\processed\tech_challenge\df_vinhos.csv', sep=';', decimal=',')
 
